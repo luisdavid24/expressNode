174687259692f31e499b1655ce9ec2d520116dd6
Luis David <david12.29@hotmail.com>
2025-01-10 22:29:46 -0500

    Agrege mis propios get de mas entidades

--- index.js
+++ index.js
@@ -38,3 +38,30 @@ app.get('/caterories/:categoryId/products/:productId', (req, res) => {
 app.listen(port, () => {
   console.log('Mi puerto ' + port);
 });
+
+//productos, categorias, ordenes de compra , usuarios, GET
+
+app.get('/purchaseOrders', (req, res) => {
+  res.json([
+    { name: 'orden1', value: 2000 },
+    { name: 'orden2', value: 3000 },
+  ]);
+});
+app.get('/purchaseOrders/:id', (req, res) => {
+  console.log(req);
+  const { id } = req.params;
+  res.json({ id, name: 'orden1' + id, value: 2000 });
+});
+
+app.get('/user', (res, req) => {
+  res.json([
+    { name: 'user1', nickName: 'nickName1' },
+    { name: 'user2', nickName: 'nickName2' },
+    { name: 'user3', nickName: 'nickName3' },
+  ]);
+});
+
+app.get('/user/:id', (res, req) => {
+  const { id } = req.params;
+  res.json({ id, name: 'user' + id, nickName: 'nickName' + id });
+});
