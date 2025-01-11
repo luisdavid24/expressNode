8bb800534f351a5dcd8af5a0a5a5ae2633ce9c8a
Luis David <david12.29@hotmail.com>
2025-01-10 23:51:41 -0500

    Implemente Quey params

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 3000;
 
@@ -10,15 +12,26 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy tu nueva ruta');
 });
 
-app.get('/productos', (req, res) => {
-  res.json([
-    { name: 'Producto 1', price: 1000 },
-    { name: 'Producto 2', price: 2500 },
-  ]);
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.urlLoremFlickr(),
+    });
+  }
+  res.json(products);
 });
 
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
 //req (request) res (response)
-app.get('/productos/:id', (req, res) => {
+app.get('/products/:id', (req, res) => {
   ///productos/:id aqui el id es un parametro por tiene : antes
   const { id } = req.params;
   res.json({
@@ -39,6 +52,18 @@ app.listen(port, () => {
   console.log('Mi puerto ' + port);
 });
 
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+}); //http://localhost:3000/users?limit=10&offset=20
+
 //productos, categorias, ordenes de compra , usuarios, GET
 
 app.get('/purchaseOrders', (req, res) => {
@@ -48,12 +73,12 @@ app.get('/purchaseOrders', (req, res) => {
   ]);
 });
 app.get('/purchaseOrders/:id', (req, res) => {
-  console.log(req);
+  //console.log(req);
   const { id } = req.params;
   res.json({ id, name: 'orden1' + id, value: 2000 });
 });
 
-app.get('/user', (res, req) => {
+app.get('/userr', (res, req) => {
   res.json([
     { name: 'user1', nickName: 'nickName1' },
     { name: 'user2', nickName: 'nickName2' },
@@ -61,7 +86,7 @@ app.get('/user', (res, req) => {
   ]);
 });
 
-app.get('/user/:id', (res, req) => {
+app.get('/userr/:id', (res, req) => {
   const { id } = req.params;
   res.json({ id, name: 'user' + id, nickName: 'nickName' + id });
 });
