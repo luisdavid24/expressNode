589021a6c071536a5e23d8667611dc1786c9ce60
Luis David <david12.29@hotmail.com>
2025-01-11 22:48:55 -0500

    Se implemento los metodos de delete and update en product

--- services/product.service.js
+++ services/product.service.js
@@ -6,7 +6,7 @@ class ProductService {
     this.generate();
   }
   generate() {
-    const limit = 100;
+    const limit = 40;
 
     for (let index = 0; index < limit; index++) {
       this.products.push({
@@ -17,15 +17,40 @@ class ProductService {
       });
     }
   }
-  create() {}
+  create(data) {
+    const newProduct = {
+      id: faker.database.mongodbObjectId(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
   find() {
     return this.products;
   }
   findOne(id) {
     return this.products.find((item) => item.id === id);
   }
-  update() {}
-  delete() {}
+  update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+    return this.products[index];
+  }
+  delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
 }
 
 module.exports = ProductService;
