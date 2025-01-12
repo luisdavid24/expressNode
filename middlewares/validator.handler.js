4e2b2a6bad80738c2af2472db3b057a662f79e3e
Luis David <david12.29@hotmail.com>
2025-01-12 17:50:10 -0500

    Comence a valir los datos que envia el cliente

---
+++ middlewares/validator.handler.js
@@ -0,0 +1,21 @@
+const { faker } = require('@faker-js/faker');
+
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const { error } = schema.validate();
+    if (error) {
+      next(boom.badRequest(error));
+    }
+    next();
+  };
+}
+
+module.exports = validatorHandler;
+
+/*
+la informacion del req se obtiene de manera diferente maneras dependiendo
+de donde la obtengamos:
+si es un post sera req.body
+si es un get  req.params
+tambien puede venir de req.query */
---
+++ schemas/products.schema.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(15);
+const price = Joi.number().integer().min(10);
+
+const createProductSchema = Joi.object({
+  name: name.require(),
+  price: price.require(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+module.exports = { createProductSchema, updateProductSchema, getProductSchema };
