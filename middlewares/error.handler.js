9c2c3e473525324d441a7c21d58b3763ef8de7aa
Luis David <david12.29@hotmail.com>
2025-01-12 15:42:44 -0500

    Cree un middleware de error

---
+++ middlewares/error.handler.js
@@ -0,0 +1,15 @@
+//Este middeleware es para detectar errores
+function logErrors(err, req, res, next) {
+  console.log(error);
+  next(err); //Crea un middleware de tipo de error
+}
+
+//Estes es para crear un formato a los errores
+function errorHandler(err, req, res, next) {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+module.exports = { logErrors };
