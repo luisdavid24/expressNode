9801fc45682dfdd197686fee9d175c04dd704604
Luis David <david12.29@hotmail.com>
2025-01-13 14:17:42 -0500

    Implemente mas schemas en products

--- routes/products.router.js
+++ routes/products.router.js
@@ -34,22 +34,31 @@ router.get(
   },
 );
 
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const newProduct = await service.create(body);
-  res.status(201).json(newProduct);
-});
-
-router.patch('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
+router.post(
+  '/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
     const body = req.body;
-    const product = await service.update(id, body);
-    res.json(product);
-  } catch (error) {
-    next(error);
-  }
-});
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  },
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
