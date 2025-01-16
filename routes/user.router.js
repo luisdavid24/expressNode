79356df78f937596b4cb26c4879145d680d99d3e
Luis David <david12.29@hotmail.com>
2025-01-16 17:02:29 -0500

    Actualice el archivo de user.router.js

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,34 +1,23 @@
 const express = require('express');
+
 const UserService = require('./../services/user.service');
 const validatorHandler = require('./../middlewares/validator.handler');
-
 const {
-  createUserSchema,
   updateUserSchema,
+  createUserSchema,
   getUserSchema,
-} = require('./../schemas/users.schema');
+} = require('./../schemas/user.schema');
 
 const router = express.Router();
 const service = new UserService();
 
-/* router.get('/other', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
   }
-}); */
-router.get('/', async (req, res) => {
-  const users = await service.find();
-
-  res.json(users);
-});
-router.get('/filterUsers', (req, res) => {
-  res.send('Yo soy un filter');
 });
 
 router.get(
@@ -37,20 +26,25 @@ router.get(
   async (req, res, next) => {
     try {
       const { id } = req.params;
-      const user = await service.findOne(id);
-      res.json(user);
+      const category = await service.findOne(id);
+      res.json(category);
     } catch (error) {
       next(error);
     }
   },
 );
+
 router.post(
   '/',
   validatorHandler(createUserSchema, 'body'),
-  async (req, res) => {
-    const body = req.body;
-    const newUser = await service.create(body);
-    res.status(201).json(newUser);
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
   },
 );
 
@@ -62,18 +56,26 @@ router.patch(
     try {
       const { id } = req.params;
       const body = req.body;
-      const user = await service.update(id, body);
-      res.json(user);
+      const category = await service.update(id, body);
+      res.json(category);
     } catch (error) {
       next(error);
     }
   },
 );
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const response = await service.delete(id);
 
-  res.json(response);
-});
+router.delete(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({ id });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 module.exports = router;
-//http://localhost:3000/users?limit=10&offset=20
