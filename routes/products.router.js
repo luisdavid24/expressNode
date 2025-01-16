c830372a10505ec8cc74d157eb06b6042a084d5b
Luis David <david12.29@hotmail.com>
2025-01-16 17:01:53 -0500

    Actualice el archivo de products.router.js

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const ProductService = require('./../services/product.service');
+
+const ProductsService = require('./../services/product.service');
 const validatorHandler = require('./../middlewares/validator.handler');
 const {
   createProductSchema,
   updateProductSchema,
   getProductSchema,
-} = require('./../schemas/products.schema');
+} = require('./../schemas/product.schema');
 
 const router = express.Router();
-const service = new ProductService();
+const service = new ProductsService();
 
-//http://localhost:3000/products?size=2 Cambiando el valor de size puedo cambiar el limit
-router.get('/', async (req, res) => {
-  const products = await service.find();
-  res.json(products);
-});
-
-router.get('/filter', (req, res) => {
-  res.send('Yo soy un filter');
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await service.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get(
   '/:id',
-  validatorHandler(getProductSchema, 'params'), //Params indica donde va encontrar la informacion
+  validatorHandler(getProductSchema, 'params'),
   async (req, res, next) => {
     try {
       const { id } = req.params;
@@ -37,10 +37,14 @@ router.get(
 router.post(
   '/',
   validatorHandler(createProductSchema, 'body'),
-  async (req, res) => {
-    const body = req.body;
-    const newProduct = await service.create(body);
-    res.status(201).json(newProduct);
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newProduct = await service.create(body);
+      res.status(201).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
   },
 );
 
@@ -60,11 +64,18 @@ router.patch(
   },
 );
 
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const response = await service.delete(id);
-
-  res.json(response);
-});
+router.delete(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({ id });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 module.exports = router;
