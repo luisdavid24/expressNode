77639de9f32acb93fbf8e1a0b26f76e5e8d2345b
Luis David <david12.29@hotmail.com>
2025-01-16 17:11:14 -0500

    Actualice category.service.js

--- services/category.service.js
+++ services/category.service.js
@@ -1,60 +1,29 @@
-const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
-class CategoriesService {
-  constructor() {
-    this.categories = [];
-    this.generate();
-  }
-  generate() {
-    const limit = 15;
-
-    for (let index = 0; index < limit; index++) {
-      this.categories.push({
-        id: faker.string.uuid(),
-        name: faker.commerce.department(),
-        description: faker.lorem.sentence(),
-      });
-    }
-  }
+class CategoryService {
+  constructor() {}
   async create(data) {
-    const newCategory = {
-      id: faker.string.uuid(),
-      ...data,
-    };
-    this.categories.push(newCategory);
-    return newCategory;
+    return data;
   }
+
   async find() {
-    return this.categories;
+    return [];
   }
+
   async findOne(id) {
-    const category = this.categories.find((item) => item.id === id);
-    if (!category) {
-      throw boom.notFound('Category not found');
-    }
-    return category;
+    return { id };
   }
+
   async update(id, changes) {
-    const index = this.categories.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new Error('Category not found');
-    }
-    const category = this.categories[index];
-    this.categories[index] = {
-      ...category,
-      ...changes,
+    return {
+      id,
+      changes,
     };
-    return this.categories[index];
   }
+
   async delete(id) {
-    const index = this.categories.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw boom.notFound('Product not found');
-    }
-    this.categories.splice(index, 1);
     return { id };
   }
 }
 
-module.exports = CategoriesService;
+module.exports = CategoryService;
