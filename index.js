ff221d7209235c7dc553930606c9509e52dfd027
Luis David <david12.29@hotmail.com>
2025-01-16 17:14:35 -0500

    Actualice index.js

--- index.js
+++ index.js
@@ -1,61 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-const routerApi = require('./routes/index.js');
-
-const app = express();
-const port = process.env.PORT || 3000;
+const routerApi = require('./routes');
 
 const {
   logErrors,
   errorHandler,
   boomErrorHandler,
-} = require('./middlewares/error.handler.js');
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-const whitelist = ['htt://localhost:8080', 'https//myapp.co'];
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('No permitido '));
+      callback(new Error('no permitido'));
     }
   },
 };
-app.use(cors(options)); //Esta es para poder informacion tipo json
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
 routerApi(app);
 
-app.listen(port, () => {
-  console.log('Mi puerto ' + port);
-});
-
-//El orden en que se ponga sera el orden de seran usado
-app.use(boomErrorHandler);
 app.use(logErrors);
+app.use(boomErrorHandler);
 app.use(errorHandler);
-/* app.get('/purchaseOrders', (req, res) => {
-  res.json([
-    { name: 'orden1', value: 2000 },
-    { name: 'orden2', value: 3000 },
-  ]);
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
 });
-app.get('/purchaseOrders/:id', (req, res) => {
-  //console.log(req);
-  const { id } = req.params;
-  res.json({ id, name: 'orden1' + id, value: 2000 });
-}); */
-
-//categorys  and ordenes de compra con un path extra api/
-
-/* app.get('/caterories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-}); */
