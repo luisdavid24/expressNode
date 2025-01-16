090152235def8985534870bd572fa001703fbc46
Luis David <david12.29@hotmail.com>
2025-01-16 16:59:42 -0500

    Actualice el archivo de index.js de routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+
 const productsRouter = require('./products.router');
-const userRouter = require('./user.router');
 const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
+const orderRouter = require('./orders.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
-  router.use('/users', userRouter);
-  router.use('/categoriesRouter', categoriesRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;
